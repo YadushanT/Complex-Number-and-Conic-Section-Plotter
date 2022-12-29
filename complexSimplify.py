5c7710a0b3e7f15c6d62f9a6613c5cfa02d9b735
YadushanT <yadushant@gmail.com>
2022-12-28 22:02:41 -0500

    complexSimplify COMPLETE

---
+++ complexSimplify.py
@@ -0,0 +1,32 @@
+import math
+
+
+#QUESTION A
+real = 1 #declaring the value for the real number
+
+imag = complex(0, 3) #declaring the value for the imaginary number
+
+exponent = complex(0, 2) #declaring the value for the exponent
+
+answer = (real + imag) ** exponent #calculating the answer according to the equation given in the question
+
+print( "Question 1a: (1+3i)²ⁱ = ", answer) #outputting answer
+print()
+
+
+
+#QUESTION B
+real = 1 #declaring the value for the real number
+
+imag = complex(0, math.sqrt(3)) #declaring the value for the imaginary number
+
+exponent = 10 #declaring the value for the exponent
+
+answer = (real + imag) ** exponent #calculating the answer according to the equation given in the question
+
+print( "Question 1b: (1 + √3i)¹⁰ⁱ = ", answer) #outputting answer
+
+
+
+
+
--- q1_group_10.py
+++
@@ -1,4 +0,0 @@
-import math
-
-a = float(input("a: "))
-b = float(input("b: "))
